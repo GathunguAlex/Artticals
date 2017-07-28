b2da88041a13bf45051d30e8de2d6dc06efe1764
Alex Griffith <alex.griffith@flatironschool.com>
2017-07-28 08:55:06 -0400

    Update adapter.rb

--- app/models/adapter.rb
+++ app/models/adapter.rb
@@ -1,4 +1,18 @@
 class Adapter
+  attr_accessor :articles
 
+  def self.read_articles_from_file(file)
+    file_string = File.read(file)
+    JSON.parse(file_string)
+  end
+
+  def initialize(file)
+    @articles = self.class.read_articles_from_file(file)
+    self.create_objects
+  end
+
+  def create_objects
+    # your code here...
+  end
 
 end
