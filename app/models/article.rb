5da2abfd8f5a3340be8b229c11eb02f3799108eb
Rachel <rachel.salois@gmail.com>
2017-08-14 11:21:30 -0400

    Update article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,4 @@
 class Article
-  attr_accessor :title, :description, :posted_at, :url, :contributor, :category
-
-
 
 # Article.find_all_by_category("Weird")
   def self.find_all_by_category(category)
