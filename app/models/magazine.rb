191fea924b054f28837b97c52ab45af9fe8ca949
GathunguAlex <alex.gathungu@student.moringaschool.com>
2022-10-30 11:11:03 +0300

    making magazine a constant

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,6 +1,6 @@
 class Magazine
-  attr_accessor :name, :category
-
+  attr_reader :name, :category
+#change from accessor to reader to make sure it cannot be changes
 
   def initialize(name, category)
     @name = name
