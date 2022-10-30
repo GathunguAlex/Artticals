5584b5014ea9ff8196f4ad9160aa83698288d242
GathunguAlex <alex.gathungu@student.moringaschool.com>
2022-10-30 14:31:34 +0300

    attr_reader

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,11 @@
 class Article
+    #article cannot change its author,mag,title so we use attr_reader
+    attr_reader :author, :magazine, :title
 
+    def initialize(author,magazine,title)
+    @author= author
+    @magazine = magazine
+    @title = title
+    end
 
 end
