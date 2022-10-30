a8fa3b7a8fef751640c2294613b88b2f23b87a20
GathunguAlex <alex.gathungu@student.moringaschool.com>
2022-10-30 13:06:39 +0300

    all method class set to array

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -2,11 +2,16 @@ class Magazine
   attr_accessor :name, :category
 #stays accessor so that it can be changed
 
+  @@all=[]
   def initialize(name, category)
     @name = name
     @category = category
-
+    # adding the intialized object to a class all..which is a variable
+    @@all << self
   end
 
-
+  #accessing  'all' using a class method
+  def self.all
+   @@all
+  end
 end
